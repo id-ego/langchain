37a0c4172d04a5e558bb277f209dfb36397ff265
ego <mrego@anakonn.com>
2024-08-18 22:12:39 +0900

    en

--- Makefile
+++ Makefile
@@ -1,29 +1,7 @@
-LANG ?= en
-
-.PHONY: set-lang
-set-lang:
-	@:
-
-.PHONY: copy-infra
-copy-infra:
-	mkdir -p _build/$(LANG)
-	cp -r data _build/$(LANG)
-	cp -r docs _build/$(LANG)
-	cp -r src _build/$(LANG)
-	cp -r static _build/$(LANG)
-	cp babel.config.js _build/$(LANG)
-	cp package.json _build/$(LANG)
-	cp yarn.lock _build/$(LANG)
-
 .PHONY: build
 build: copy-infra
-	cd _build/$(LANG)
-	yarn
 	yarn build
 	mv build v0.2
 	mkdir build
 	mv v0.2 build
 	mv build/v0.2/404.html build
-
-%:
-	@:
--- ignore-step.sh
+++ ignore-step.sh
@@ -3,21 +3,10 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-
-if [ "$VERCEL_ENV" == "production" ] || [ "$VERCEL_GIT_COMMIT_REF" == "main" ] || [ "$VERCEL_GIT_COMMIT_REF" == "v0.1" ]; then 
+if [ "$VERCEL_ENV" == "production" ] || [ "$VERCEL_GIT_COMMIT_REF" == "main" ]; then 
     echo "✅ Production build - proceeding with build"
-    exit 1; 
-else 
-    echo "Checking for changes in docs/ and templates/:"
-    echo "---"
-    git log -n 50 --pretty=format:"%s" -- . ../templates | grep -v '(#'
-    if [ $? -eq 0 ]; then
-        echo "---"
-        echo "✅ Changes detected in docs/ or templates/ - proceeding with build"
-        exit 1
-    else
-        echo "---"
-        echo "🛑 No changes detected in docs/ or templates/ - ignoring build"
-        exit 0
-    fi
+    exit 1
 fi
+
+echo "🛑 Not a production build - ignoring build"
+exit 0
